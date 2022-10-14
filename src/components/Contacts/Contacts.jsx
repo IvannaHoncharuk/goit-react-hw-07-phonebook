ab648ca4e11d116d77fe5d96d71e28271304ea4e
Ivanna Honcharuk <Ivannatsyganjuk@gmail.com>
2022-10-14 23:39:39 +0200

    fix filter

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,16 +2,16 @@ import { ContactsItem } from './ContactItem/ContactItem';
 import { Contacts } from './Contacts.styled';
 import { useSelector } from 'react-redux';
 import {selectContactsFilter} from '../../redux/slice';
-import { useGetContactsQuery } from 'services/api';
+import { useGetContactsQuery } from '../../services/api';
 
 export const ContactsList = () => {
   const { data: contacts} = useGetContactsQuery();
   const filter = useSelector(selectContactsFilter);
-  const filteredContacts = contacts.filter(({ name }) => name.toLowerCase().includes(filter));
+  const filteredContacts = contacts?.filter(({ name }) => name.toLowerCase().includes(filter));
 
   return (
     <Contacts>
-      {filteredContacts.length ? (
+      {filteredContacts?.length ? (
         filteredContacts.map(({ id, name, number }) => (
           <ContactsItem
             key={id}
